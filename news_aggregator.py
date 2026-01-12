ddacf7522c44abf755ad28774ef518faeaf73ff1
ahmeddiabate-droid <ahmeddiabate@gmail.com>
2026-01-12 14:35:20 -0500

    Update news_aggregator.py

--- news_aggregator.py
+++ news_aggregator.py
@@ -20,7 +20,7 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 
 opts = Options()
-service = service (r"C:\Users\ahmed.laminediabate\OneDrive - LifeWorks\Desktop\chrome-win32\chrome.exe")
+service = Service (r"C:\Users\ahmed.laminediabate\OneDrive - LifeWorks\Desktop\chrome-win32\chrome.exe")
 driver = webdriver.Chrome(service=service, options =opts)
 
 from urllib.parse import urljoin, quote
