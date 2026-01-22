8f0d5af3a71480c73ca99e687f05536863f133f4
ahmeddiabate-droid <ahmeddiabate@gmail.com>
2026-01-22 11:51:11 -0500

    Update news_aggregator.py

--- news_aggregator.py
+++ news_aggregator.py
@@ -239,7 +239,6 @@ class NewsAggregator:
             href = self._fix_relative_url(link['href'],base_url)
 
          # Retraite Quebec usually has the date in a <time> or nearby
-         date_str = None
          detail_div=h2.find_next_sibling("div",class_="detail")
          date_span = detail_div.find("span", class_ = "layout-actualites-date")
          date_str= date_span.get_text(strip=True) if date_span else None
